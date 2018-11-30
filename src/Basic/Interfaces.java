fdd9280c1dd734175458c6e4220bf0ce2e2ab272
softcrylic <softcrylic.testautomation@gmail.com>
2018-11-30 20:22:33 +0530

    New Changes

--- src/Basic/Interfaces.java
+++ src/Basic/Interfaces.java
@@ -1,27 +1,43 @@
 package Basic;
 
-interface calc
-{
-	int VAR=1;
+interface calc {
+	int VAR = 1;
+
 	void calc(int item);
-	
-}
-interface calculate extends calc
-{			 
-	
-            int VAR = 0;
-                      
-            void calc(int item);
+
 }
 
-        
-// Only one time you can override the interfcae and We need output like 0 0 4.
-//Note you can use the Array also.
+interface calculate extends calc {
 
-public abstract class Interfaces implements calc  {
-	
-	  public static void main(String args[]) 
-      {
-       
-      }
+	int VAR = 0;
+
+	void calc(int item);
+}
+
+abstract class display implements calculate {
+	int x;
+
+	public void cal(int item) {
+
+		if (item < 2) {
+			x = VAR;
+
+			calc(x);
+		} else
+			x = item * item;
+	}
+
+}
+
+// By calling the Cal function Print:- 0 0 4 in console
+public class Interfaces extends display {
+	public static void main(String args[]) {
+
+	}
+
+	@Override
+	public void calc(int item) {
+		// TODO Auto-generated method stub
+
+	}
 }
